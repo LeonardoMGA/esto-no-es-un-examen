c1c293116f1b3ffe90b1d9295e94f7e43b3cac3d
leonardo <leonardomartin.alapizco@gmail.com>
2019-07-11 07:26:27 -0600

    sadas

--- examen1.cpp
+++ examen1.cpp
@@ -1,1 +1,86 @@
-hola cesar
+//Eliminar siempre y cuando existencia es 0
+#include"funciones.h"
+typedef struct{
+	int cod; //unico
+	char des[25];//Desc del articulo
+	char fecreg[11];//fecha de regitro
+	int exi;//existencia del art
+	int codpro; //codigo de proveedor
+	bool reg;
+	
+}Articulo;
+
+Articulo invent[10];
+
+int t =10;
+
+/*
+1 agregar  - si codigo es unico
+2 eliminar - si existencia es 0;
+3 consultar
+	1 especific
+	2 general
+	3 por proveedor
+	4 regresar
+4 ordernar
+	1 por codigo
+	2 por fecha de registro
+	3 regresar
+*/
+
+FILE *arc;
+int init(){	
+	if((arc=fopen("C:\\archivos\\tienda.txt","a+b"))!=NULL){
+		rewind(arc);
+		fread(&invent,sizeof(Articulo),10,arc);
+		fclose(arc);
+		return 1;
+	}else {
+		puts("Error de fichero :(");
+		return 0;
+	}
+}
+
+void save(){
+	if((arc=fopen("C:\\archivos\\tienda.txt","w+"))!=NULL){
+		fwrite(&invent,sizeof(Articulo),10,arc);
+		fclose(arc);
+	}
+}
+
+
+void agregar(){
+	for(int x=0; x)
+}
+
+
+int mainMenu(){
+	while(1){
+		int op;
+		system("cls");
+		puts("1)Agregar");
+		puts("1)Eliminar");
+		puts("1)Consultar");
+		puts("1)Ordenar");
+		puts("1)Salir");
+		puts("---------------------------------------");
+		printf("Ingrese una opcion: ");
+		scanInt(&op);
+		system("cls");
+		switch(op){
+			case 1:{
+				
+				break;
+			}
+		}
+	}
+	
+	
+}
+
+main(){
+	if(init()){
+		mainMenu();
+	}
+}
+
---
+++ funciones.h
@@ -0,0 +1,169 @@
+#include<stdio.h>
+#include<conio.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<windows.h>
+
+typedef struct{
+	int mn,mx;
+}Range;
+
+Range nums = {'0','9'};
+Range letras = {'a','z'};
+
+
+void scanRange(char *cad, int t, Range range){
+	int x=0;
+	while(1){
+		char aux;
+		char c=getch();
+		aux=tolower(c);
+		if(aux>=range.mn && aux<=range.mx && x<t){
+			*(cad + x)=c;
+			printf("%c",c);
+			x++;
+		}else if(aux==8 && x>0){
+			x--;
+			printf("\b \b");
+			*(cad + x)=' ';
+		}else if(aux==13){
+			*(cad + x)='\0';
+			printf("\n");
+			break;	
+		}
+	}
+}
+
+
+void scanInt(int *retVal){
+	int t=9;
+	char num[t];
+	int x=0;
+	while(1){
+		char c =getch();
+		if(c>='0' && c<='9' && x<t){
+			printf("%c",c);
+			num[x]=c;
+			x++;
+		}else if(c==8 && x>0){
+			x--;
+			printf("\b \b");
+			num[x]=' ';
+		}else if(c==13) {
+			//num[x]='\0';
+			*retVal = atoi(num);
+			printf("\n");
+			break;	
+		}
+		
+	}
+}
+
+
+void scanAlpha(char *cad,int t){
+	
+	int x=0;
+	while(1){
+		char c=getch();
+		if((c>='a' && c<='z' || c>='A' && c<='Z' || c==' ' || GetAsyncKeyState(192)==-32767) && x<t){
+			*(cad + x)=c;
+			printf("%c",c);
+			x++;
+		}else if(c==8 && x>0){
+			x--;
+			printf("\b \b");
+			*(cad + x)=' ';
+		}else if(c==13){
+			*(cad + x)='\0';
+			printf("\n");
+			break;		
+		}
+	}
+}
+
+void valifec(char *pfecha)
+{	int dia=0, mes=0, anio=0, x=0, c=0, d=0, m=0, a=0, fec=0, bi=0;
+	char diac[2], mesc[2], anioc[4];
+	do
+	{ 	x=d=m=a=0;
+		strcpy(diac,"  ");
+		strcpy(mesc,"  ");
+		strcpy(anioc,"    ");
+		do
+		{	c=getch();
+	    	if(c>=48 && c<=57)
+    		{ 	printf("%c",c);
+    			if(x<2)
+    			{	diac[d]=c;
+        			d++;
+        		}
+				else if (x<5)
+				{	mesc[m]=c;
+					m++;
+				}
+				else
+				{	anioc[a]=c;
+					a++;
+				}
+				*(pfecha+x)=c;
+    	    	x++;
+	    	}
+	    	if(x==2 || x==5)
+	    	{	printf("/");
+    	    	*(pfecha+x)='/';
+        		x++;        
+        	} 
+			if(c==8 && x>0)
+	    	{	x--;
+	    		if(x==2)
+	    		{	printf("\b\b  \b\b");
+	    			*(pfecha+x)=' ';
+	    			x--;
+	    			*(pfecha+x)=' ';
+	    			d--;
+	    		}
+	    		else if(x<2)
+	    		{	printf("\b \b");
+					*(pfecha+x)=' ';
+					d--;
+				}
+	    		else if(x==5)
+	    		{	printf("\b\b  \b\b");
+	    			*(pfecha+x)=' ';
+	    			x--;
+					*(pfecha+x)=' ';
+					m--;
+	    		}
+	    		else if(x<5)
+	    		{	printf("\b \b");
+					*(pfecha+x)=' ';
+					m--;
+				}
+	    		else if(x>5)
+	    		{	printf("\b \b");
+	    			*(pfecha+x)=' ';
+	    			a--;
+	    		}
+    		}            
+		}while(x<10);
+		dia=atoi(diac);
+		mes=atoi(mesc);
+		anio=atoi(anioc);
+		bi=0;
+		if(anio%4==0 && (anio%100!=0 || anio%400==0))
+			bi++;
+		if(dia<=31 && (mes==1 || mes==3 || mes==5 || mes==7 || mes==8 || mes==10 || mes==12) && anio>=1900 && anio<=2050)
+			fec=1;
+		else if(dia<=30 && (mes==4 || mes==6 || mes==9 || mes==11) && anio>=1900 && anio<=2050)
+			fec=1;
+		else if(dia<=28 && mes==2 && anio>=1900 && anio<=2050)
+			fec=1;
+		else if(dia==29 && mes==2 && anio>=1900 && anio<=2050 && bi==1)
+			fec=1;
+		else
+			for(int y=1;y<=10;y++)
+				printf("\b \b");
+	}while(fec!=1);
+}
+
+
